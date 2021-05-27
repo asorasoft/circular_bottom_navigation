0f36c88908bc7183235f979633e2fa648ac3817f
Sor Thyrith <sor.thyrith@gmail.com>
2021-05-27 10:46:32 +0700

    set tabitem label style to nullable

--- lib/tab_item.dart
+++ lib/tab_item.dart
@@ -3,7 +3,7 @@ class TabItem {
   IconData icon;
   String title;
   Color circleColor;
-  TextStyle labelStyle;
+  TextStyle? labelStyle;
 
   TabItem(this.icon, this.title, this.circleColor, {this.labelStyle = const TextStyle(fontWeight: FontWeight.bold)});
 }
